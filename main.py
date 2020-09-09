5a464ac80095b56a5e49f16eb54fb343af623f8f
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-09-09 23:00:53 +0500

    added function for getting data from csv file

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import matplotlib.pyplot as pyplot
+import csv
 
-pyplot.plot([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
-pyplot.show()
+# pyplot.plot([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
+# pyplot.show() Отображение таблицы
+
+def getDataFromCsv(filename):
+
+	with open(filename, 'r', encoding='utf-8') as file:
+
+		data = csv.reader(file)
